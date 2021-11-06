48e75b8f772371cd13e7f5a9fb7ef9acac33ec5c
Atha Hibatullah <athalayana@mail.ugm.ac.id>
2021-11-06 18:40:26 +0700

    Added artifact list

--- rater.py
+++ rater.py
@@ -1,7 +1,21 @@
 import json
 
-file_json = open("substat_list.json")
+class Artifact:
+    def __init__(self, set, piece, rarity, main_stat, lvl, sub):
+        self.set = set
+        self.piece = piece
+        self.rarity = rarity
+        self.main_stat = main_stat
+        self.lvl = lvl
+        self.sub = sub
 
-data = json.loads(file_json.read())
+# file_json = open("Artifact_list.json")
+substat_json = open("substat_list.json")
+artifact_json = open("Artifact_list.json")
+data_substat = json.loads(substat_json.read())
+data_artifact = json.loads(artifact_json.read())
 
-print(data['Ampas']['HP'])
+# Substat = Artifact(0,0,0,0,0,[data['Bloodstained Chivalry']['Flower'],data['Bloodstained Chivalry']['Feather'],data['Bloodstained Chivalry']['Eon'],data['Bloodstained Chivalry']['Goblet'],data['Bloodstained Chivalry']['Circlet']])
+# Substat = Artifact(0,0,0,0,0,data_substat['Ampas']['HP'][5])
+
+print(Substat.sub)
